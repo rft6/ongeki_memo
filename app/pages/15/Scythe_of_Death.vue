c7e7de5957810943e2bef7bcb19d9a22d413ff12
rft6 <rft6.0724@gmail.com>
2025-04-21 20:07:07 +0900

    Scythe of Death 修正

--- app/pages/15/Scythe_of_Death.vue
+++ app/pages/15/Scythe_of_Death.vue
@@ -4,14 +4,14 @@
         <ImageGallery
             title="28小節目"
             comment="左手2連を意識"
-            image1="/images/15/Scythe_of_Death_028_before.webp"
-            image2="/images/15/Scythe_of_Death_028_after.webp"
+            image1="/images/15/Scythe_of_Death/Scythe_of_Death_028_before.webp"
+            image2="/images/15/Scythe_of_Death/Scythe_of_Death_028_after.webp"
             />
         <ImageGallery
-            title="トリルも同じ形のまま押せることを忘れない"
-            comment="最後"
-            image1="/images/15/Scythe_of_Death_034_before.webp"
-            image2="/images/15/Scythe_of_Death_034_after.webp"
+            title="34小節目"
+            comment="トリルも同じ形のまま押せることを忘れない"
+            image1="/images/15/Scythe_of_Death/Scythe_of_Death_034_before.webp"
+            image2="/images/15/Scythe_of_Death/Scythe_of_Death_034_after.webp"
             />
     </div>
 </template>
