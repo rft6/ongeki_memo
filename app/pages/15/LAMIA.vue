ae9d3fe51c90ef14bdbf832b0bbc3a7f43f46c86
rft6 <rft6.0724@gmail.com>
2025-04-22 12:01:00 +0900

    LAMIA コメント修正

--- app/pages/15/LAMIA.vue
+++ app/pages/15/LAMIA.vue
@@ -3,7 +3,7 @@
         <h3>LAMIA [MASTER 15]</h3>
         <ImageGallery
             title="17小節目"
-            comment="左手は全押し"
+            comment="右手は全押し"
             image1="/images/15/LAMIA/LAMIA_017_before.webp"
             image2="/images/15/LAMIA/LAMIA_017_after.webp"
             />
