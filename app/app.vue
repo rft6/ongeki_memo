39c339e3d45342c01c2eb207480290f285205151
rft6 <rft6.0724@gmail.com>
2025-05-06 23:07:17 +0900

    Googleアクセス解析追加

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,23 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup>
+useHead({
+  script: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-6943NMVM10',
+      async: true
+    },
+    {
+      children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-6943NMVM10');
+      `,
+      type: 'text/javascript'
+    }
+  ]
+})
+</script>
