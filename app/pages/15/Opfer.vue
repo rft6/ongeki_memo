e2a209fc68813310fd82a2572e212e237d12a67d
rft6 <rft6.0724@gmail.com>
2025-05-08 15:24:48 +0900

    Opfer 追加

--- app/pages/15/Opfer.vue
+++ app/pages/15/Opfer.vue
@@ -7,5 +7,11 @@
             image1="/images/15/Opfer/Opfer_054_before.webp"
             image2="/images/15/Opfer/Opfer_054_after.webp"
             />
+        <ImageGallery
+            title="83小節目"
+            comment="巻き込んでHITになりやすいから左始動階段の赤だけ右手で取る"
+            image1="/images/15/Opfer/Opfer_083_before.webp"
+            image2="/images/15/Opfer/Opfer_083_after.webp"
+            />
     </div>
 </template>
