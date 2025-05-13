7e8399e88d1ac93a762a5a1fc80947a312a82f9b
rft6 <rft6.0724@gmail.com>
2025-05-14 02:00:44 +0900

    Titania 画像変更

--- app/pages/14plus/Titania.vue
+++ app/pages/14plus/Titania.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        <h3>Don't Fight The Music [MASTER 14+]</h3>
+        <h3>Titania [MASTER 14+]</h3>
         <ImageGallery
             title="105小節目"
             comment="最後は両手で同時押し"
-            image1="/images/14+/Titania_105_before.webp"
-            image2="/images/14+/Titania_105_after.webp"
+            image1="/images/14+/Titania/Titania_105_before.webp"
+            image2="/images/14+/Titania/Titania_105_after.webp"
             />
     </div>
 </template>
